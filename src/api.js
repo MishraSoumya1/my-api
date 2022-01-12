01881e8ce339cccb0111722aeb8dbfb477237922
Soumya Ranjan Mishra <miki@Soumyas-MacBook-Air.local>
2022-01-13 00:56:58 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -2,12 +2,25 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const express = require("express");
 const serverless = require("serverless-http");
+const sessions = require("express-session");
 
 const app = express();
 const router = express.Router();
 
+const oneDay = 1000 * 60 * 60 * 24;
+
 app.use(cookieParser());
 app.use(cors());
+
+//session middleware
+app.use(
+  sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay, secure },
+    resave: false,
+  })
+);
 
 router.get("/", (req, res) => {
   res.status(200).cookie("cookieName", "I am netlify").json({
