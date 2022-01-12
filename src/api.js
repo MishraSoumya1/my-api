e8243d6c9d175a52f0c427bd62e66f94d1ce2c62
Soumya Ranjan Mishra <miki@Soumyas-MacBook-Air.local>
2022-01-13 00:32:57 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -4,9 +4,12 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
+app.use(cookieParser());
+app.use(cors());
+
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!",
+  res.status(200).cookie("cookieName", "I am netlify").json({
+    value: Math.random().toString(),
   });
 });
 
